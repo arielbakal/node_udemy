42252f1333431fcbdd94fcefdcfa071a20f53d71
Ariel Bakal <arielbakal.dev@gmail.com>
2023-01-23 17:33:47 -0300

    35 unfinished

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,12 +1,21 @@
 const { crearArchivo } = require('./helpers/multiplicar.js');
+const argv = require('yargs')
+                .option('b', {
+                    alias: 'base',
+                    type: 'number',
+                }).argv;
+
 
 console.clear();
+
+console.log( argv );
+
+console.log('La base es:', argv.base);
+
 
 //const [ , , arg3 ] = process.argv;
 //const [ , base ] = arg3.split('=');
 
- 
-
-crearArchivo( base )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch( err => console.log(err) );
+//crearArchivo( base )
+//    .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
+//    .catch( err => console.log(err) );
